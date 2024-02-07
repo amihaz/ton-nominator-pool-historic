a51f98faee3777a5e05b5b82c7a29b39024b3cb4
Shahar Yakir <shahar.yakir@gmail.com>
2024-02-07 16:02:11 +0000

    added latest block + validator amount

--- ton-nominator-pool-historic.js
+++ ton-nominator-pool-historic.js
@@ -1,17 +1,39 @@
-const {TonClient4, Address, fromNano} = require("ton")
+const { TonClient4, Address, fromNano } = require("ton")
 
-const NominatorPoolAddress = 'Ef-ow0xjskANzvDgPQ6ClnIwcBi4F3N3WPF_pLTvoTVS7In9';
+const NominatorPoolAddress = 'Ef94U_9atKuZ2m98i578qrDaEc0-uUFNqVStvQs_Pwq9RDs7';
 const DateSnapshot = '2023.12.31';
 
-async function main() {
+const clientV4 = new TonClient4({ endpoint: 'https://mainnet-v4.tonhubapi.com' });
 
-    const clientV4 = new TonClient4({endpoint: 'https://mainnet-v4.tonhubapi.com'}); 
+async function main() {
+    await stakeAtDay(NominatorPoolAddress, DateSnapshot);
+    await latestStake(NominatorPoolAddress);
+}
+
+async function stakeAtDay(address, yyyy_mm_dd) {
     const epoch = Math.floor(new Date(DateSnapshot).getTime() / 1000);
     const blockNumber = (await clientV4.getBlockByUtime(epoch)).shards[0].seqno;
+    console.log('Stake at:', yyyy_mm_dd, ' block:', blockNumber);
+    await stake(address, blockNumber);
+}
 
-    let res = await clientV4.runMethod(blockNumber, Address.parse(NominatorPoolAddress), 'list_nominators');
+async function latestStake(address) {
+    const latestBlock = (await clientV4.getLastBlock()).last.seqno;
+    console.log('Latest stake;', ' block:', latestBlock);
+    await stake(address, latestBlock);
+}
 
-    console.log(fromNano(res.result[0].items[0].items[1].value));
+async function stake(address, blockNumber) {
+    let res = await clientV4.runMethod(blockNumber, Address.parse(address), 'list_nominators');
+    let resPoolData = await clientV4.runMethod(blockNumber, Address.parse(address), 'get_pool_data');
+
+    const nominatorStake = fromNano(res.result[0].items[0].items[1].value)
+    const validatorAmount = fromNano(resPoolData.result[3].value)
+
+    console.log('Nominator stake:', nominatorStake, 'TON');
+    console.log('Validator amount:', validatorAmount, 'TON');
+    console.log('Total:', parseFloat(nominatorStake) + parseFloat(validatorAmount), 'TON');
+    console.log('-----------')
 }
 
 main();
